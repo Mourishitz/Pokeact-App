272c079c98f18e0dbe061205d5cdf914c66cc12e
Mourishitz <gabrielmorishita@hotmail.com>
2022-10-21 23:30:13 -0300

    ✨ Tela home iniciada com sucesso

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,43 @@
-import Header from "../../components/Header"
-import Core from "../Core"
+import axios from "axios"
+import Core from "../Core";
+import * as React from 'react'
+import { useState, useEffect } from "react"
+import {Skeletons} from "../../components/Skeletons";
+import {Grid} from "@mui/material";
+import PokemonCard from "../../components/PokemonCard";
 
-export default function Home(): JSX.Element {
+
+export default function Home() {
+    const [pokemons, setPokemons] = useState<any[]>([])
+
+    useEffect(()=>{
+            getPokemons()
+        }, []
+    )
+
+    const getPokemons = () => {
+        let endpoints = []
+        for (let i = 1; i < 100; i++){
+            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+        }
+        axios.all(endpoints.map((endpoints) => axios.get(endpoints))).then((res: any[]) => {
+            setPokemons(res);
+        })
+    }
+
     return (
         <Core>
-            <p>Um Pikachu Selvagem apareceu!</p>
+            <Grid container spacing={0}>
+            {pokemons.length === 0 ? (
+                <Skeletons />
+            ) : (
+                pokemons.map((pokemon, key) => (
+                    <Grid item xs={12} sm={6} md={4} lg={2} key={key} className="cardDisplay">
+                        <PokemonCard name={pokemon.data.name} image={pokemon.data.sprites.front_default} types={pokemon.data.types}/>
+                    </Grid>
+                ))
+            )}
+            </Grid>
         </Core>
     )
 }
